a21dd3b4b07939517426f7573ed3f50664587e17
Stanleyashz <okaforobinna547@gmail.com>
2026-03-15 21:14:17 -0400

    launch interface of the prototype

--- SmartTaskApp.swift
+++ SmartTaskApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct SmartTaskApp: App {
+    @StateObject private var taskStore = TaskStore()
+
     var body: some Scene {
         WindowGroup {
             LaunchScreenView()
+                .environmentObject(taskStore)
         }
     }
 }
